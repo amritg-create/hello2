0e285087fa76ae91f979b03734638b5dc961610c
amritg-create <76961652+amritg-create@users.noreply.github.com>
2024-07-09 16:11:39 -0400

    Update ciscotest.py

--- ciscotest.py
+++ ciscotest.py
@@ -25,6 +25,106 @@ import yaml
 import uuid
 
 
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+import pandas as pd
+import yaml
+from pprint import pprint
+
+
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+
+# PDF Loader 
+
+loader = PyPDFLoader('cisco_pdfs\Q3 Cisco Report.pdf')
+
+# THIS TAKES 5 MINUTES...
+documents = loader.load()
+
+CHUNK_SIZE = 1000
+
+text_splitter = CharacterTextSplitter(
+    chunk_size=CHUNK_SIZE, 
+    # chunk_overlap=100,
+    separator="\n"
+)
+
+docs = text_splitter.split_documents(documents)
+
+docs
+
+len(docs)
+
+docs[0]
+
+pprint(dict(docs[5])["page_content"])
+
+docs[5]
+
+# Vector Database
+
+embedding_function = OpenAIEmbeddings(
+    model='text-embedding-ada-002',
+    api_key=OPENAI_API_KEY
+)
+
+vectorstore = Chroma.from_documents(
+    docs, 
+    persist_directory="Cisco_Q3summarizer/data/chroma_cisco_learning.db",
+    embedding=embedding_function
+)
+
+
+vectorstore = Chroma(
+    persist_directory="Cisco_Q3summarizer/data/chroma_cisco_learning.db",
+    embedding_function=embedding_function
+)
+
+retriever = vectorstore.as_retriever()
+
+retriever
+
+# RAG LLM Model
+
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+model = ChatOpenAI(
+    model = 'gpt-3.5-turbo',
+    temperature = 0.7,
+    api_key=OPENAI_API_KEY
+)
+
+rag_chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | model
+    | StrOutputParser()
+)
+
+result = rag_chain.invoke("What was Cisco total revenues in third quarter?")
+
+pprint(result)
+
+
+result = rag_chain.invoke("What was Cisco product revenues in third quarter?")
+
+pprint(result)
+
+
+
 # Initialize the Streamlit app
 st.set_page_config(page_title="Your Cisco Q3 Earnings Statement Copilot", layout="wide")
 st.title("Your Cisco Earnings Copilot")

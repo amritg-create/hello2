725087e3e35061126ef1ca2ed40fb547839d0e17
amritg-create <76961652+amritg-create@users.noreply.github.com>
2024-07-09 16:02:06 -0400

    Update ciscotest.py

--- ciscotest.py
+++ ciscotest.py
@@ -30,7 +30,7 @@ st.set_page_config(page_title="Your Cisco Q3 Earnings Statement Copilot", layout
 st.title("Your Cisco Earnings Copilot")
 
 # Load the API Key securely
-import os
+
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
 # Set up Chat Memory
